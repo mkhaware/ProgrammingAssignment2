ef9a203e0db78d99c99145710b189027fc80c13d
Manish Khaware <manish.khaware@gmail.com>
2019-12-24 10:56:20 -0500

    Coursera R Programming Week 3 submission

--- cachematrix.R
+++ cachematrix.R
@@ -4,6 +4,29 @@
 ## Write a short comment describing this function
 
 makeCacheMatrix <- function(x = matrix()) {
+  #This function takes a matrix and creates a cache
+  cacheMatrix <- NULL
+  
+  #define setMatrix method 
+  setMatrix <- function(y) {
+    x <<- y
+    cacheMatrix <<- NULL
+  }
+  
+  #define getMatrix method that returns matrix 'x'
+  getMatrix <- function() x
+  
+  #define setCache method that updates cacheMatrix with inverse
+  setCache <- function(inverse) cacheMatrix <- inverse
+  
+  #define getCache method that returns the inverse of matrix 'x'
+  getCache <- function() cacheMatrix
+  
+  # list methods exposed through this function
+  list(setMatrix = setMatrix,
+       getMatrix = getMatrix,
+       setCache = setCache,
+       getCache = getCache)
 
 }
 
@@ -12,4 +35,18 @@ makeCacheMatrix <- function(x = matrix()) {
 
 cacheSolve <- function(x, ...) {
         ## Return a matrix that is the inverse of 'x'
+  cacheMatrix <- x$getCache()
+  
+  #if cacheMatrix is not null then return cacheMatrix
+  if (!is.null(cacheMatrix)) {
+    message("loading cache matrix")
+    return(cacheMatrix)
+  }
+  #if it is null then use createCacheMatrix function to set the value and then return
+  else {
+    myMatrix <- x$getMatrix()
+    cacheMatrix <- solve(myMatrix, ...)
+    x$setCache(cacheMatrix)
+    return(cacheMatrix)
+  }
 }
